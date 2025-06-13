b8190151e859a471bf0586133b35861832a00327
leandrochs <leandrochagask@gmail.com>
2025-06-13 15:34:02 -0300

    chore: configurações iniciais da tela, estado do jogo e controle de som

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
-print("Hello World!!")
+from pgzero.actor import Actor
+from pygame import Rect
+from random import randint
+
+screen: any  # type: ignore
+keyboard: any  # type: ignore
+mouse: any  # type: ignore
+music: any  # type: ignore
+sounds: any  # type: ignore
+
+WIDTH = 800
+HEIGHT = 600
+is_music_enabled = True
+is_sound_enabled = True
